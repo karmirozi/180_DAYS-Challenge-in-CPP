0f2633393d465b2cfa842065dc6f45448056e862
Rozi karmi <150064340+karmirozi@users.noreply.github.com>
2024-03-22 01:12:37 +0530

    Update 9.While and Do while Loop.cpp

--- BASICS/9.While and Do while Loop.cpp
+++ BASICS/9.While and Do while Loop.cpp
@@ -1,1 +1,57 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the number n: " << endl;
+    cin >> n;
 
+//    intialize;
+//    while(break){
+//        update
+//    };
+
+// while loop
+    int i1 = 1;
+    while (i1 <= n){
+        cout << i1 << " ";
+        i1++;
+    }
+    cout << endl;
+// table with while loop
+    int i2 = 1;
+    while (i2 <= 10){
+        cout << n << " * " << i2 << " is " << i2*n << endl;
+        i2++;
+    }
+
+//    initialize;
+//    do{
+//    update
+//    };
+//    while(break);
+
+//    do while
+    cout << "with do while loop" << endl;
+    int x = 1;
+    do {
+        cout << x << " ";
+        x++;
+}
+    while (x<=n);
+    cout  << endl;
+
+//    break and continue only works in for loop
+//    switch(expression) {
+//        case x:
+//            code block
+//            break;
+//        case y:
+//            code block
+//            break;
+//        default:
+//            code block
+//    }
+
+//    scope of variable you know this from javascript
+//    first find in local then search global
+}
