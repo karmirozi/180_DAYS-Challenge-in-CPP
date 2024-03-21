adfa1620940856f85c67e0cb915bdb0f820c4181
Rozi karmi <150064340+karmirozi@users.noreply.github.com>
2024-03-22 01:11:32 +0530

    Update 5.Pattern Printing.cpp

--- BASICS/5.Pattern Printing.cpp
+++ BASICS/5.Pattern Printing.cpp
@@ -115,24 +115,6 @@ int main(){
 
 
 
-//SOME MORE PATTERN PROBLES
-
-
-
-
-#include <iostream>
-using namespace std;
-int main() {
-    int n;
-    cout << "Enter the number n: " << endl;
-    cin >> n;
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= n-i+1; j++) {
-            cout << "*" << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n-i+1; j++) {
@@ -169,43 +151,7 @@ int main() {
     }
     cout << endl;
 
-    for (int i = 1; i <= n; i++) {
-        char SomeChar1 = 'A';
-        for (int j = 1; j <= n-i+1; j++) {
-            cout << SomeChar1 << " ";
-            SomeChar1++;
-        }
-        cout << endl;
-    }
-    cout << endl;
-
-
-    for (int i = 1; i <= n; i++) {
-        // space
-        for (int j = 1; j <= n-i; j++) {
-            cout << " ";
-        }
-        // char
-        char SomeChar1 = 'A';
-        int breakpoint = (2*i-1)/2;
-        for (int j = 1; j <= 2*i-1 ; j++) {
-            if(j <= breakpoint) {
-                cout << SomeChar1;
-                SomeChar1++;
-            }
-            else{
-                cout << SomeChar1;
-                SomeChar1--;
-            }
-        }
-        // number
-        for (int j = 1; j <= n-i; j++) {
-            cout << " ";
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
+   
 
 
 
