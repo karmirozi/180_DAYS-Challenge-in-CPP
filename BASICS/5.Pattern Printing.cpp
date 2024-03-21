8928e4e297931990de4ccc6d9933a883939f01ee
Rozi karmi <150064340+karmirozi@users.noreply.github.com>
2024-03-22 01:07:32 +0530

    Update 5.Pattern Printing.cpp

--- BASICS/5.Pattern Printing.cpp
+++ BASICS/5.Pattern Printing.cpp
@@ -1,1 +1,223 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    // print pattern
+    int n;
+    cin >> n;
+    // nested loop
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout<<"*"<<" " ; // or cout<<"* ";
+        }
+        cout << endl;
+    }
+    // 1st row(i) = 1 and (j) is column
+    // 2nd row <= 5
+    // 3rd print * 5 times
+    // 4th row = row + 1
+
+    cout << endl;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout<<"10 " ;
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << i <<" " ;
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    int x;
+    cout << "Enter x:" << endl;
+    cin >> x;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << x <<" " ;
+        }
+        cout << endl;
+        x++;
+    }
+
+    cout << endl;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = n; j >= 1; j--) {
+            cout << j << " " ;
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << j*j << " " ;
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    char a = 'a';
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << a << " " ;
+        }
+        cout << endl;
+        a++;
+    }
+
+    cout << endl;
+
+    for (int i = 1; i <= n; i++) {
+        for (char j = 'a'; j <= 'e'; j++) {
+            cout << j << " " ;
+        }
+        cout << endl;
+    }
+    //or
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            a = a+j-1;
+            cout << a << " " ;
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    int count = 1;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << count << " " ;
+            count++;
+        }
+        cout << endl;
+    }
+    //or
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << (i-1)*5+j << " " ;
+        }
+        cout << endl;
+    }
+}
+
+
+
+//SOME MORE PATTERN PROBLES
+
+
+
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the number n: " << endl;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n-i+1; j++) {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n-i+1; j++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = n; j >= n-i+1; j--) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    int spaces = 2*(n-1);
+    for (int i = 1; i <= n; i++) {
+        // number
+        for (int j = 1; j <= i; j++) {
+            cout << j;
+        }
+        // space
+        for (int j = 1; j <= spaces; j++) {
+            cout << " ";
+        }
+        // number
+        for (int j = i; j >= 1; j--) {
+            cout << j;
+        }
+        cout << endl;
+        spaces-=2;
+    }
+    cout << endl;
+
+    for (int i = 1; i <= n; i++) {
+        char SomeChar1 = 'A';
+        for (int j = 1; j <= n-i+1; j++) {
+            cout << SomeChar1 << " ";
+            SomeChar1++;
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+
+    for (int i = 1; i <= n; i++) {
+        // space
+        for (int j = 1; j <= n-i; j++) {
+            cout << " ";
+        }
+        // char
+        char SomeChar1 = 'A';
+        int breakpoint = (2*i-1)/2;
+        for (int j = 1; j <= 2*i-1 ; j++) {
+            if(j <= breakpoint) {
+                cout << SomeChar1;
+                SomeChar1++;
+            }
+            else{
+                cout << SomeChar1;
+                SomeChar1--;
+            }
+        }
+        // number
+        for (int j = 1; j <= n-i; j++) {
+            cout << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
