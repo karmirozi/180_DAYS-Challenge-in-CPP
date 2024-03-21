310cd15071d838c0f075024649492b268c87e4d5
Rozi karmi <150064340+karmirozi@users.noreply.github.com>
2024-03-22 01:09:15 +0530

    Update 7.Advanced Pattern Problem.cpp

--- BASICS/7.Advanced Pattern Problem.cpp
+++ BASICS/7.Advanced Pattern Problem.cpp
@@ -1,1 +1,64 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cout << "Enter the number n: " << endl;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        // spaces
+        for (int j = 1; j <= n-i; j++) {
+            cout << "  ";
+        }
+        // * printing
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    cout << endl;
 
+    for (int i = 1; i <= n; i++) {
+        // spaces
+        for (int j = 1; j <= n-i; j++) {
+            cout << "  ";
+        }
+        // * printing
+        for (int j = 1; j <= i; j++) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int i = 1; i <= n; i++) {;
+        char someChar1 = 'A';
+        // spaces
+        for (int j = 1; j <= n-i; j++) {
+            cout << "  ";
+        }
+        // * printing
+        for (int j = 1; j <= i; j++) {
+            cout << someChar1 << " ";
+            someChar1++;
+        }
+        //or
+        //for (char j = 'A'; j <= 'A'+i-1 ; j++) {
+        //cout << j << " ";
+        //}
+        cout << endl;
+    }
+    cout << endl;
+
+    for (int i = 1; i <= n; i++) {
+        // spaces
+        for (int j = 1; j <= n-i; j++) {
+            cout << "  ";
+        }
+        // * printing
+        for (int j = i; j >= 1; j--) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
